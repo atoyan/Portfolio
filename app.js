44bc951506d11d62850f7d7b803e4f492d0235b3
WC ando <atoyanandranik@gmail.com>
2019-02-08 17:16:22 +0200

    added bootstrap and index.ejs

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+app = express();
+PORT = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + "/public"));
+app.use("/scripts", express.static(__dirname + "/public/scripts/"));
+app.use("/stylesheets", express.static(__dirname + "/public/stylesheets/"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/download", (req, res) => {
+  let file = __dirname + "/public/files/CV.pdf";
+  res.download(file);
+});
+app.listen(PORT, err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Server Running on ${PORT}`);
+  }
+});
